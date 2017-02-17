905b6ab1bb98daffad39cc7cef54ba6d67595afc
Sergey Skvortsov <skvortsov-serge@gmail.com>
2017-02-17 18:21:39 +0200

    added fixed header, made navigation

--- js/app.js
+++ js/app.js
@@ -9,8 +9,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var wScroll = document.body.scrollTop;
         var position = 1600 + wScroll;
         var bg = document.querySelector('.parallax-bg');
-        // bg.style.backgroundPosition = '-1650px -'+position/3 +'px';
-        bg.style.backgroundPosition = '-325px -'+position/2 +'px';
+        bg.style.backgroundPosition = '-325px -' + position / 2 + 'px';
+        // --------------------------------------------------------------
+
+        var header = document.querySelector('.header-width');
+        if (document.body.scrollTop >= 165) {
+            document.querySelector('.about-me').style.marginTop = '200px';
+            header.classList.add('scroll-header');
+        } else {
+            document.querySelector('.about-me').style.marginTop = '0px';
+            header.classList.remove('scroll-header');
+        }
+
     }
+
+    var el = document.getElementsByClassName('t-menu')[0];
+    el.addEventListener('click', function(e) {
+        e.preventDefault();
+        var id = e.target.getAttribute('href');
+        var element = document.querySelector(id);
+        var header = document.querySelector('header');
+        var top = element.offsetTop - 90;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
 
 });
